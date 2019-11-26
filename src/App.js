7fcc04a70b996b1458c716da88c518391a46c47c
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-26 13:49:33 +0000

    react dice game complete - score, reset and hold functions added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Img3 from './Components/Img/dice3.png'
 import Img4 from './Components/Img/dice4.png'
 import Img5 from './Components/Img/dice5.png'
 import Img6 from './Components/Img/dice6.png'
+import Arrow from './Components/Img/arrow.png'
 import './App.css';
 
 
@@ -13,47 +14,50 @@ export default class App extends Component {
   state = {
     imgs: [
       {
-        img: './Components/Img/dice1.png',
+        img: Img1,
         value:1
       },
       {
-        img: './Components/Img/dice2.png',
+        img: Img2,
         value:2
       },
       {
-        img: './Components/Img/dice3.png',
+        img: Img3,
         value:3
       },
       {
-        img: './Components/Img/dice4.png',
+        img: Img4,
         value:4
       },
       {
-        img: './Components/Img/dice5.png',
+        img: Img5,
         value:5
       },
       {
-        img: './Components/Img/dice6.png',
+        img: Img6,
         value:6
       }
     ],
     players: [
       {
         id: 1,
-        name: "",
+        name: "Matt",
         score: 0,
         wins: 0,
         hold: false
       },
       {
         id: 2,
-        name: "",
+        name: "Ttam",
         score: 0,
         wins: 0,
         hold: false
       },
     ], 
-    currDie: ""
+    currDie: {
+      img: Arrow,
+      value: 0
+    },
   }
 
   randomNum = ()=> {
@@ -67,10 +71,87 @@ export default class App extends Component {
           })
   }
 
+  reset = () => {
+    this.setState({
+      players: [
+        {
+          id: 1,
+          name: "Matt",
+          score: 0,
+          wins: 0,
+          hold: false
+        },
+        {
+          id: 2,
+          name: "Ttam",
+          score: 0,
+          wins: 0,
+          hold: false
+        },
+      ], 
+      currDie: {
+        img: Arrow,
+        value: 0
+      },
+    })
+  }
+
+  
+  switchPlayers = () => {
+    if(this.state.players[1].hold == false){
+      let newArray = [...this.state.players];
+      newArray = [newArray[1], newArray[0]]
+      this.setState({
+        players: newArray,
+      })
+    }
+  }
+
+  addScore = () => {
+    const newArray = [...this.state.players];
+    newArray[0].score+= this.state.currDie.value;
+    this.setState({
+      players: newArray,
+    })
+  }
+
+  hold = () => {
+    const newArray = [...this.state.players];
+    newArray[0].hold = true;
+    this.setState({
+      players: newArray,
+    })
+    this.switchPlayers();
+  }
+
+  checkScores = () => {
+    this.state.players.map((item, i)=> {
+      if(item.score>= 21){
+
+        let newArray = [...this.state.players];
+        newArray[i].wins = newArray[i].wins +1;
+
+        for (let i = 0; i < newArray.length; i++) {
+          newArray[i].score = 0;
+          newArray[i].hold = false;
+        }
+
+        this.setState({
+          players: newArray,
+        })
+      }
+    })
+  }
+
   roll = async () => {
     await this.calcRoll();
     console.log(this.state.currDie) //returning correct info but not showing img in render
+    this.addScore();
+    this.checkScores()
+    this.switchPlayers();
   }
+
+
 
 
   render() {
@@ -78,21 +159,41 @@ export default class App extends Component {
 
     <div className="App">
         <div id="gameBoard">
-          <div className = "playerRead text">
-
+          <div className = "turnRead text">
+            <h2>{this.state.players[0].name}, it's your turn!!</h2>
           </div>
           <div className = "die">
-            <img url= {this.state.currDie.img} />
+            <img className="dieImg" src= {this.state.currDie.img} alt="die image" />
           </div>
-          <button onClick={this.roll} >Roll!</button>
+          <div>
+            <button onClick={this.roll} >Roll!</button>
+            <button onClick={this.hold} >Hold!</button>
+            <button onClick={this.reset} >Reset!</button>
+          </div>
           <div className = "scoreRead">
-            <h2 className = "p1Name"></h2>
+            <h2 className = "p1Name">{this.state.players.map((item)=> {
+              if(item.id === 1) return item.name })}</h2>
             <div className = "p1Score scores ">
-
+              <p>Score: </p>
+            {this.state.players.map((item)=> {
+              if(item.id === 1) return item.score })}
             </div>
-            <h2 className = "p2Name"></h2>
-            <div className = "p2Score scores ">
-              
+            <div className = "p1Wins scores">
+              <p>Wins: </p>
+              {this.state.players.map((item)=> {
+              if(item.id === 1) return item.wins })}
+            </div>
+            <h2 className = "p2Name">{this.state.players.map((item)=> {
+              if(item.id === 2) return item.name })}</h2>
+            <div className = "p2Score scores">
+              <p>Score: </p>
+            {this.state.players.map((item)=> {
+              if(item.id === 2) return item.score })}
+            </div>
+            <div className = "p2Wins scores">
+            <p>Win: </p>
+              {this.state.players.map((item)=> {
+              if(item.id === 2) return item.wins })}
             </div>
           </div>
         </div>
